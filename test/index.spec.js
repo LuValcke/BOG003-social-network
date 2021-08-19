6387b93888fe5aa87564e14ae89888e13ca6225f
Lu Valcke <lfvalcke@gmail.com>
2021-08-19 13:15:54 -0500

    Se realizan pruebas de test para signIn y Google

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,49 @@
 // importamos la funcion que vamos a testear
+import { createUser, /* signIn */ signInWithGoogle } from '../src/lib/index.js';
 
-import { signIn } from '../src/lib/index.js';
+const firebaseMock = require('firebase-mock');
 
-describe('signIn', () => {
-  it('Debería retornar error en password', (done) => {
-    signIn('juesdavajovi2426@gmail.com', '123456').catch((error) => {
-      expect(error.code).toBe('auth/wrong-password');
-      done();
+const mockAuth = new firebaseMock.MockAuthentication();
+
+const mockSdk = new firebaseMock.MockFirebaseSdk(
+  () => null,
+  () => mockAuth,
+);
+mockSdk.auth().autoFlush();
+global.firebase = mockSdk;
+
+describe('createUser', () => {
+  it('Deberia ser una funcion', () => {
+    expect(typeof (createUser)).toBe('function');
+  });
+  it('Deberia poder crear un nuevo usuario', async () => {
+    createUser('laboratoria@gmail.com', '12345678');
+
+    await mockSdk.auth().getUserByEmail('laboratoria@gmail.com').then((result) => {
+      expect(result.email).toBe('laboratoria@gmail.com');
+      expect(typeof result).toBe('object');
     });
   });
+});
+
+/* describe('signIn', () => {
+  it('Deberia ser una funcion', () => {
+    expect(typeof (signIn)).toBe('function');
+  });
+  it('Deberia poder iniciar sesion', async () => {
+    signIn('laboratoria@gmail.com', '12345678');
+    await mockSdk.auth().onAuthStateChanged((user) => user.email)
+      .then((result) => {
+        expect(result.email).toBe('laboratoria@gmail.com');
+      });
+  });
+}); */
+
+describe('signInWithGoogle', () => {
+  it('Deberia ser una funcion', () => {
+    expect(typeof (signInWithGoogle)).toBe('function');
+  });
+  it('Deberia poder iniciar sesion con Google', () => signInWithGoogle().then((result) => {
+    expect(typeof result).toBe('object');
+  }));
 });
