01c9ded4bde929bea44797ea00e5d49e407fd383
Lorraine Gelis <lgelisdiaz@outlook.com>
2021-08-27 12:31:49 -0500

    Se ajusta el evento boton publicar

--- src/lib/view/templateFeed.js
+++ src/lib/view/templateFeed.js
@@ -1,10 +1,15 @@
 import {
-  createPost, getPosts, onGetPost, deletePost, getPost, updatePost,
+    createPost,
+    getPosts,
+    onGetPost,
+    deletePost,
+    getPost,
+    updatePost,
 } from '../index.js';
 
 export const feed = () => {
-  // Esta variable almacena la porción de html a adjuntar en el body
-  const viewFeed = `
+    // Esta variable almacena la porción de html a adjuntar en el body
+    const viewFeed = `
         <header class="header-feed">
           <img class="logo-feed" src="img/logo.png"> 
           <div class="icons">
@@ -31,31 +36,47 @@ export const feed = () => {
         </footer>
       `;
     // Aquí se crea el div contenedor donde se adjunta la variable viewLogin
-  const main = document.createElement('div');
-  main.className = 'feed';
-  main.innerHTML = viewFeed;
+    const main = document.createElement('div');
+    main.className = 'feed';
+    main.innerHTML = viewFeed;
 
-  const btnPostMobile = main.querySelector('.imgPost');
+    const btnPostMobile = main.querySelector('.imgPost');
 
-  btnPostMobile.addEventListener('click', () => {
-    main.querySelector('.displayPost').style.display = 'block';
-  });
+    btnPostMobile.addEventListener('click', () => {
+        main.querySelector('.displayPost').style.display = 'block';
+    });
 
-  let editStatus = false;
+    let editStatus = false;
 
-  let id = '';
+    let id = '';
 
-  const btnPost = main.querySelector('#postButton');
+    const btnPost = main.querySelector('#postButton')
 
-  const post = main.querySelector('.inputPost');
+    const post = main.querySelector('.inputPost');
 
-  const postContainer = main.querySelector('.postContainer');
+    const postContainer = main.querySelector('.postContainer');
 
-  onGetPost(() => {
-    getPosts().then((querySnapshot) => {
-      postContainer.innerHTML = '';
-      querySnapshot.forEach((doc) => {
-        postContainer.innerHTML += `
+    btnPost.addEventListener('click', async() => {
+        if (!editStatus) {
+            await createPost(post.value);
+            console.log("hola");
+        } else {
+            console.log(id);
+            await updatePost(id, {
+                post: post.value,
+            });
+            editStatus = false;
+            id = '';
+            btnPost.innerText = 'Publicar';
+        }
+        post.value = '';
+        post.focus();
+    });
+    onGetPost(() => {
+        getPosts().then((querySnapshot) => {
+            postContainer.innerHTML = '';
+            querySnapshot.forEach((doc) => {
+                postContainer.innerHTML += `
         <div class='post'>
           <img class="imgProfile" src="./img/imgProfile.png">
           <h3 id="userName">Amandine Perenceja </h3>
@@ -65,41 +86,25 @@ export const feed = () => {
           <img class="like" title="Me gusta" src="./img/like.png">
         </div>
         `;
-        const btnDelete = main.querySelectorAll('.deletePost');
-        btnDelete.forEach((btn) => {
-          btn.addEventListener('click', async (e) => {
-            await deletePost(e.target.dataset.id);
-          });
-        });
-        const btnEdit = main.querySelectorAll('.editPost');
-        btnEdit.forEach((btn) => {
-          btn.addEventListener('click', async (e) => {
-            const docPost = await getPost(e.target.dataset.id);
-            post.value = docPost.data().post;
-            btnPost.innerText = 'Editar';
-            editStatus = true;
-            id = docPost.id;
-          });
-        });
-
-        btnPost.addEventListener('click', async () => {
-          if (!editStatus) {
-            await createPost(post.value);
-          } else {
-            console.log(id);
-            await updatePost(id, {
-              post: post.value,
+                const btnDelete = main.querySelectorAll('.deletePost');
+                btnDelete.forEach((btn) => {
+                    btn.addEventListener('click', async(e) => {
+                        await deletePost(e.target.dataset.id);
+                    });
+                });
+                const btnEdit = main.querySelectorAll('.editPost');
+                btnEdit.forEach((btn) => {
+                    btn.addEventListener('click', async(e) => {
+                        const docPost = await getPost(e.target.dataset.id);
+                        post.value = docPost.data().post;
+                        btnPost.innerText = 'Editar';
+                        editStatus = true;
+                        id = docPost.id;
+                    });
+                });
             });
-            btnPost.innerText = 'Publicar';
-            editStatus = false;
-            id = '';
-          }
-          post.value = '';
-          post.focus();
         });
-      });
     });
-  });
 
-  return main;
+    return main;
 };
